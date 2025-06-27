8781a220927b6182c575c7198d277523ec53ba50
Patrick Dunagan <patrickmdunagan@yahoo.com>
2025-06-27 14:44:36 -0500

    updartes

--- src/pages/services.js
+++ src/pages/services.js
@@ -14,11 +14,11 @@ export default function Services() {
                     Our Services
                 </Typography>
                 <Typography variant="body1" color="text.secondary">
-                    At ShipUSA, we pride ourselves on offering a full range of essential services designed to make your life easier.
-                    From reliable shipping and receiving to secure private mailboxes, passport photos, fingerprinting, and notary
-                    services — we’re your one-stop destination for both personal and business needs. Our team is known for being
-                    friendly, helpful, and always willing to go the extra mile. Whether you’re dropping off a package, making copies,
-                    or renting a U-Haul, you’ll always be greeted with a smile and a commitment to top-notch service.
+                    {"At ShipUSA, we pride ourselves on offering a full range of essential services designed to make your life easier."}
+                    {" From reliable shipping and receiving to secure private mailboxes, passport photos, fingerprinting, and notary"}
+                    {" services — we’re your one-stop destination for both personal and business needs. Our team is known for being"}
+                    {" friendly, helpful, and always willing to go the extra mile. Whether you’re dropping off a package, making copies,"}
+                    {" or renting a U-Haul, you’ll always be greeted with a smile and a commitment to top-notch service."}
                 </Typography>
             </Box>
             <ShippingPartners />

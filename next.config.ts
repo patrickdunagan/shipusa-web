b03ebf2af94dd9113647c5f1cb49dd75e61bdf67
Patrick Dunagan <patrickmdunagan@yahoo.com>
2025-06-27 15:00:39 -0500

    updartes

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  output: "export", // 👈 this enables static export
+  
 };
 
 export default nextConfig;

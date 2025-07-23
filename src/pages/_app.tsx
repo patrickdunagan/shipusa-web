2f0d58db96ca5b832249118d0234e25f40ddae4f
Patrick Dunagan <patrickmdunagan@yahoo.com>
2025-07-23 17:21:56 -0500

    updates

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,7 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-        
+
         {/* Primary Meta Tags */}
         <title>ShipUSA & More - Shipping, U-Haul, Mailbox & Business Services</title>
         <meta
@@ -28,7 +28,7 @@ export default function App({ Component, pageProps }: AppProps) {
         />
         <meta name="author" content="ShipUSA & More" />
         <meta name="robots" content="index, follow" />
-        
+
         {/* Open Graph / Facebook */}
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://shipusaandmore.com/" />
@@ -40,7 +40,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta property="og:image" content="https://shipusaandmore.com/assets/stock-flag.jpg" />
         <meta property="og:site_name" content="ShipUSA & More" />
         <meta property="og:locale" content="en_US" />
-        
+
         {/* Twitter */}
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://shipusaandmore.com/" />
@@ -50,13 +50,13 @@ export default function App({ Component, pageProps }: AppProps) {
           content="Authorized shipping center for all major carriers. U-Haul rentals, private mailboxes, notary services, and professional business solutions."
         />
         <meta property="twitter:image" content="https://shipusaandmore.com/assets/stock-flag.jpg" />
-        
+
         {/* Geo Tags */}
         <meta name="geo.region" content="US" />
         <meta name="geo.placename" content="Your City, State" />
         <meta name="geo.position" content="latitude;longitude" />
         <meta name="ICBM" content="latitude, longitude" />
-        
+
         {/* Business Schema Markup */}
         <script
           type="application/ld+json"
@@ -86,7 +86,7 @@ export default function App({ Component, pageProps }: AppProps) {
                 "@type": "OpeningHoursSpecification",
                 "dayOfWeek": [
                   "Monday",
-                  "Tuesday", 
+                  "Tuesday",
                   "Wednesday",
                   "Thursday",
                   "Friday"
@@ -105,7 +105,7 @@ export default function App({ Component, pageProps }: AppProps) {
                 "geoMidpoint": {
                   "@type": "GeoCoordinates",
                   "latitude": 35.65359983540524,
-                "longitude": -97.57191458036665
+                  "longitude": -97.57191458036665
                 },
                 "geoRadius": "25000"
               },
@@ -122,7 +122,7 @@ export default function App({ Component, pageProps }: AppProps) {
                     }
                   },
                   {
-                    "@type": "Offer", 
+                    "@type": "Offer",
                     "itemOffered": {
                       "@type": "Service",
                       "name": "U-Haul Rentals",
@@ -132,7 +132,7 @@ export default function App({ Component, pageProps }: AppProps) {
                   {
                     "@type": "Offer",
                     "itemOffered": {
-                      "@type": "Service", 
+                      "@type": "Service",
                       "name": "Mailbox Services",
                       "description": "Private and digital mailbox rentals"
                     }
@@ -150,28 +150,24 @@ export default function App({ Component, pageProps }: AppProps) {
             })
           }}
         />
-        
-        {/* Google Search Console Verification - Replace with your actual verification code */}
-        <meta name="google-site-verification" content="your-google-search-console-verification-code" />
-        
-        {/* Bing Webmaster Tools - Replace with your actual verification code */}
-        <meta name="msvalidate.01" content="your-bing-verification-code" />
-        
+
+      
+
         {/* Canonical URL */}
         <link rel="canonical" href="https://shipusaandmore.com/" />
-        
+
         {/* Preconnect to external domains */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       </Head>
-      
+
       {/* Google Analytics Scripts */}
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-X2WXQDFXH3"
         strategy="afterInteractive"
       />
       <Script id="gtag-init" strategy="afterInteractive">
-       {`
+        {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
@@ -180,7 +176,7 @@ export default function App({ Component, pageProps }: AppProps) {
           });
         `}
       </Script>
-      
+
       <Component {...pageProps} />
     </>
   );
